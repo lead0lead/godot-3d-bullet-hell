5a865505602c9822c1f44899ab5f6031de1465e7
lead0lead <159436295+lead0lead@users.noreply.github.com>
2025-01-23 17:20:33 +0100

    Improve camera controller
    camera now only smoothly follows the player along the cameras local x and y axis, while the position along its local z axis remains fixes

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -2,7 +2,7 @@ extends Camera3D
 
 @export_range(0.0, 0.1) var camera_sensitivity := 0.0025
 
-@export var max_cam_distance := 3.0
+@export var max_cam_distance := 5.0
 
 @onready var _camera_pivot: Node3D = %CameraPivot
 @onready var _player := _camera_pivot.get_parent()
@@ -29,14 +29,23 @@ func _physics_process(delta: float) -> void:
 	_camera_input_direction = Vector2.ZERO
 	
 	# handle camera positon
-
-	var vector_to_player: Vector3 = _player.position - _camera_pivot.position
+	
+	var vector_to_player: Vector3 = (_player.global_position - 
+		_camera_pivot.global_position)
 	
 	if vector_to_player.length() > max_cam_distance:
 		_camera_pivot.position = (_player.position 
 			- vector_to_player.normalized() 
 			* max_cam_distance)
 	else:
-		_camera_pivot.position = lerp(_camera_pivot.position, _player.position, 0.05)
-
+		_camera_pivot.position = lerp(_camera_pivot.position,
+			 _player.position, 0.05)
 	
+	var vector_from_player: Vector3 = (_camera_pivot.position 
+		- _player.position)
+		
+	var local_z_offset = (_camera_pivot.global_basis.z 
+		* vector_from_player.dot(_camera_pivot.global_basis.z))
+		
+	if local_z_offset.length() > 0:
+		_camera_pivot.position = _camera_pivot.position - local_z_offset
