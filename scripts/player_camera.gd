b953b3ce50bdd0a7ef57ff00f8dfb5a6f4fa55a2
lead0lead <159436295+lead0lead@users.noreply.github.com>
2025-01-23 14:37:48 +0100

    Improve camera tracking
    camera now smoothly tracks the player

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -2,23 +2,41 @@ extends Camera3D
 
 @export_range(0.0, 0.1) var camera_sensitivity := 0.0025
 
+@export var max_cam_distance := 3.0
+
 @onready var _camera_pivot: Node3D = %CameraPivot
+@onready var _player := _camera_pivot.get_parent()
 
 var _camera_input_direction := Vector2.ZERO
 
 func _unhandled_input(event: InputEvent) -> void:
 	var is_camera_motion := (
 		event is InputEventMouseMotion and 
-		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
-	)
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED)
 	
 	if is_camera_motion:
 		_camera_input_direction = event.screen_relative * camera_sensitivity
 		
 func _physics_process(delta: float) -> void:
+	
+	# handle camera rotation
+	
 	_camera_pivot.rotation.x += _camera_input_direction.y
 	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, 
 		-PI / 2.0, PI / 2.0)
 	_camera_pivot.rotation.y -= _camera_input_direction.x
 
 	_camera_input_direction = Vector2.ZERO
+	
+	# handle camera positon
+
+	var vector_to_player: Vector3 = _player.position - _camera_pivot.position
+	
+	if vector_to_player.length() > max_cam_distance:
+		_camera_pivot.position = (_player.position 
+			- vector_to_player.normalized() 
+			* max_cam_distance)
+	else:
+		_camera_pivot.position = lerp(_camera_pivot.position, _player.position, 0.05)
+
+	
