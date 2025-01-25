95817978eb7298d16f47742a42d0db12a941c929
lead0lead <159436295+lead0lead@users.noreply.github.com>
2025-01-25 18:42:17 +0100

    Add rocket boost
    Add rocket-boost type mechanic that allows players to  fly into camera direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,8 @@ extends CharacterBody3D
 @export var rotation_speed := 8.0
 @export var jump_impulse := 14.0
 @export var dash_impulse := 30.0
+@export var flight_speed := 20.0
+@export var flight_acceleration := 120.0
 
 @onready var _camera: Camera3D = %PlayerCamera
 
@@ -17,37 +19,49 @@ var _gravity := -50
 var flying: bool = false
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("fly"):
+		flying = true
+	else:
+		flying = false
+		
 	_rotate_player_model(delta)
 	_handle_movement(delta)
 
 func _handle_movement(delta):
 	
-	if Input.is_action_pressed("jump") and not is_on_floor() and velocity.y <= 0:
-		velocity.y *= 0.2
-	
 	var player_input_direction = Input.get_vector("left", "right"
-			, "forward", "back")
+		, "forward", "back")
 	
 	var camera_forward = _camera.global_basis.z
 	var camera_right = _camera.global_basis.x
 
-	_move_direction = camera_forward * player_input_direction.y + camera_right * player_input_direction.x
-	_move_direction.y = 0.0
-	_move_direction = _move_direction.normalized()
+	if not flying:
+		if Input.is_action_pressed("jump") and not is_on_floor() and velocity.y <= 0:
+			velocity.y *= 0.2
+		_move_direction = camera_forward * player_input_direction.y + camera_right * player_input_direction.x
+		_move_direction.y = 0.0
+		_move_direction = _move_direction.normalized()
 
-	var y_velocity := velocity.y
-	velocity.y = 0.0
-	
-	velocity = velocity.move_toward(_move_direction * movement_speed
-			, acceleration * delta)
-	
-	velocity.y = y_velocity + _gravity * delta
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_impulse
-	
-	if Input.is_action_just_pressed("dash"):
-		_dash()
+		var y_velocity := velocity.y
+		velocity.y = 0.0
+		
+		velocity = velocity.move_toward(_move_direction * movement_speed
+				, acceleration * delta)
+		
+		velocity.y = y_velocity + _gravity * delta
+		
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_impulse
+		
+		if Input.is_action_just_pressed("dash"):
+			_dash()
+
+	else:
+		_move_direction = ( -camera_forward 
+			+ camera_right * 
+			player_input_direction.x)
+		velocity = velocity.move_toward(_move_direction * flight_speed, flight_acceleration * delta)
+
 	move_and_slide()
 
 	print("current speed: ", velocity.length())
@@ -65,3 +79,4 @@ func _dash():
 	var y_velocity = velocity.y
 	velocity = velocity.normalized() * (velocity.length() + dash_impulse)
 	velocity.y = y_velocity
+	
