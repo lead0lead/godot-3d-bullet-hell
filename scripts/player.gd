403975461e3efa7e9159dac0e1a6652cb77b4aaa
lead0lead <159436295+lead0lead@users.noreply.github.com>
2025-01-25 14:02:20 +0100

    Add dash jump-boost
    jumping while dashing now gives a speedboost

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,14 +9,20 @@ extends CharacterBody3D
 @export var rotation_speed := 8.0
 @export var jump_impulse := 14.0
 @export var flight_impulse := 8.0
-@export var dash_impulse := 30.0
+@export var dash_impulse := 40.0
 
 @onready var _camera: Camera3D = %PlayerCamera
 
 var _move_direction = Vector3.ZERO
 var _last_move_direction := Vector3.FORWARD
 var _gravity := -50
+
 var flying: bool = false
+
+var dashing: bool = false
+var dash_velocity = Vector3.ZERO
+var dash_timer := 0.0
+var dash_duration := 0.2
 
 func _physics_process(delta: float) -> void:
 	_rotate_player_model(delta)
@@ -44,9 +50,17 @@ func _handle_movement(delta):
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_impulse
+		velocity += dash_velocity
 	
 	if Input.is_action_just_pressed("dash"):
 		_dash()
+		
+	if dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			dashing = false
+			dash_velocity = Vector3.ZERO
+
 	move_and_slide()
 
 	print("current speed: ", velocity.length())
@@ -80,6 +94,9 @@ func _handle_flight():
 		pass
 
 func _dash():
+	dashing = true
+	dash_velocity = velocity.normalized() * (velocity.length() + dash_impulse)
 	var y_velocity = velocity.y
-	velocity = velocity.normalized() * (velocity.length() + dash_impulse)
+	velocity = dash_velocity
 	velocity.y = y_velocity
+	dash_timer = dash_duration
