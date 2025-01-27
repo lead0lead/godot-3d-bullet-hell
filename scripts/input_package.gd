0f987c1f89fdfda0830d0211beb81b9cb5c06077
lead0lead <159436295+lead0lead@users.noreply.github.com>
2025-01-27 21:02:52 +0100

    Refactor player character state machine
    refactor state machine to a more simplified structure for the time being
    focus on simple architecture for fast prototyping

--- scripts/input_package.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name InputPackage
-
-var actions: Array[String]
-var combat_actions: Array[String]
-
-var input_direction: Vector2
--- scripts/player.gd
+++ scripts/player.gd
@@ -20,10 +20,91 @@ class_name Player
 var _move_direction = Vector3.ZERO
 var _last_move_direction := Vector3.FORWARD
 var gravity := -50
+var applied_gravity := gravity
+
 var boosting: bool = false
 var flying: bool = false
 
+enum States {IDLE, WALKING, JUMPING, GLIDING, BOOSTING, DASHING, FLYING_IDLE, FLYING, FALLING}
+
+var state := States.IDLE
+var previous_state := state
+
 func _physics_process(delta: float) -> void:
+	
+	var player_input_direction = Input.get_vector("left", "right"
+		, "forward", "back")
+	
+	var camera_forward = _camera.global_basis.z
+	var camera_right = _camera.global_basis.x
+	
+	velocity.y = velocity.y + gravity * delta
+	
+	if state == States.FALLING:
+		if is_on_floor():
+			set_state(States.IDLE)
+	
+	if velocity.length() == 0:
+		if state not in [States.FLYING_IDLE, States.IDLE]:
+			set_state(States.IDLE)
+		else:
+			set_state(States.FLYING_IDLE)
+
+	if not is_on_floor() and state not in [States.BOOSTING, States.DASHING]:
+		if state not in [States.FLYING_IDLE, States.IDLE]:
+			if Input.is_action_just_pressed("toggle-flight"):
+				set_state(States.FALLING)
+		else:
+			set_state(States.FALLING)
+
+	if state == States.JUMPING:
+		velocity.y += jump_impulse
+		set_state(States.FALLING)
+
+	if state == States.IDLE and player_input_direction != Vector2.ZERO:
+		set_state(States.WALKING)
+
+	if state == States.FLYING_IDLE and player_input_direction != Vector2.ZERO:
+		set_state(States.FLYING)
+
+	if is_on_floor() and state in [States.IDLE, States.WALKING]:
+		if Input.is_action_just_pressed("jump"):
+			set_state(States.JUMPING)
+
+	if state == States.FALLING and Input.is_action_pressed("jump"):
+		set_state(States.GLIDING)
+		
+	if Input.is_action_pressed("boost"):
+		set_state(States.BOOSTING)
+		
+	if state == States.BOOSTING and Input.is_action_just_released("boost"):
+		set_state(previous_state)
+	
+	if state not in [States.BOOSTING] and Input.is_action_just_pressed("dash"):
+		set_state(States.DASHING)
+		
+	## DASH STATE HERE ##
+	
+	if state not in [States.BOOSTING] and Input.is_action_just_pressed("toggle-flight"):
+		set_state(States.FLYING_IDLE)
+	
+	
+
+	if ((state not in [States.JUMPING
+			, States.BOOSTING, States.FLYING
+			, States.FLYING_IDLE]) 
+			and is_on_floor() 
+			and Input.is_action_pressed("jump")):
+		set_state(States.GLIDING)
+	
+	if state == States.BOOSTING:
+		_move_direction = ( -camera_forward 
+			+ camera_right * 
+			player_input_direction.x)
+		velocity = velocity.move_toward(_move_direction 
+			* flight_speed, flight_acceleration 
+			* delta)
+	
 	if Input.is_action_pressed("boost"):
 		boosting = true
 	else:
@@ -39,7 +120,7 @@ func _handle_movement(delta):
 	
 	var camera_forward = _camera.global_basis.z
 	var camera_right = _camera.global_basis.x
-
+	
 	if not boosting:
 		if Input.is_action_pressed("jump") and not is_on_floor() and velocity.y <= 0:
 			velocity.y *= glide_speed
@@ -85,3 +166,17 @@ func _dash():
 	velocity = velocity.normalized() * (velocity.length() + dash_impulse)
 	velocity.y = y_velocity
 	
+func set_state(new_state: int) -> void:
+	previous_state = state
+	state = new_state
+	
+	if previous_state in [States.GLIDING, States.FLYING_IDLE, States.FLYING]:
+		applied_gravity = gravity
+	
+	if state == States.GLIDING:
+		applied_gravity = gravity * glide_speed
+	
+	if state in [States.FLYING, States.FLYING_IDLE]:
+		applied_gravity = 0
+
+	print(state)
