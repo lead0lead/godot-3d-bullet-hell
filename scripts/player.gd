1b7d6e24a74aaf3c525c4bbd47f37233107504f2
lead0lead <159436295+lead0lead@users.noreply.github.com>
2025-01-27 23:22:29 +0100

    Fix issues with player state machine
    fix various bugs
    add further functionality

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,29 +6,33 @@ class_name Player
 @export_group("Movement")
 @export var movement_speed := 10.0
 @export var acceleration := 80.0
-@export var rotation_speed := 8.0
-@export var jump_impulse := 14.0
-@export var dash_impulse := 30.0
-@export var flight_speed := 20.0
-@export var flight_acceleration := 120.0
-@export var glide_speed := 0.4
+@export var flight_speed := 7.5
+@export var flight_acceleration := 60.0
+@export var boost_speed := 20.0
+@export var boost_acceleration := 120.0
 
-@export var air_movement_speed := 7.5
+@export var fligt_ascension_speed := flight_speed * 10
+@export var flight_descension_speed := - flight_speed * 10
+
+@export var rotation_speed := 8.0
+@export var jump_impulse := 20.0
+@export var dash_speed := 30.0
+@export var glide_speed := 0.5
 
 @onready var _camera: Camera3D = %PlayerCamera
 
-var _move_direction = Vector3.ZERO
-var _last_move_direction := Vector3.FORWARD
-var gravity := -50
-var applied_gravity := gravity
-
-var boosting: bool = false
-var flying: bool = false
-
 enum States {IDLE, WALKING, JUMPING, GLIDING, BOOSTING, DASHING, FLYING_IDLE, FLYING, FALLING}
-
 var state := States.IDLE
 var previous_state := state
+
+var _move_direction = Vector3.ZERO
+var _last_move_direction := Vector3.FORWARD
+
+var gravity := -110.0
+var applied_gravity := gravity
+
+var applied_movement_speed := movement_speed
+var applied_acceleration := acceleration
 
 func _physics_process(delta: float) -> void:
 	
@@ -37,29 +41,23 @@ func _physics_process(delta: float) -> void:
 	
 	var camera_forward = _camera.global_basis.z
 	var camera_right = _camera.global_basis.x
-	
-	velocity.y = velocity.y + gravity * delta
-	
+
 	if state == States.FALLING:
 		if is_on_floor():
 			set_state(States.IDLE)
 	
 	if velocity.length() == 0:
-		if state not in [States.FLYING_IDLE, States.IDLE]:
+		if state not in [States.FLYING_IDLE, States.FLYING]:
 			set_state(States.IDLE)
 		else:
 			set_state(States.FLYING_IDLE)
 
-	if not is_on_floor() and state not in [States.BOOSTING, States.DASHING]:
-		if state not in [States.FLYING_IDLE, States.IDLE]:
-			if Input.is_action_just_pressed("toggle-flight"):
-				set_state(States.FALLING)
-		else:
-			set_state(States.FALLING)
-
-	if state == States.JUMPING:
-		velocity.y += jump_impulse
+	if not is_on_floor() and state not in [States.BOOSTING, States.DASHING, States.FLYING_IDLE, States.FLYING]:
 		set_state(States.FALLING)
+
+	if state == States.GLIDING:
+		if Input.is_action_just_released("jump"):
+			set_state(States.FALLING)
 
 	if state == States.IDLE and player_input_direction != Vector2.ZERO:
 		set_state(States.WALKING)
@@ -77,80 +75,50 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		set_state(States.BOOSTING)
 		
-	if state == States.BOOSTING and Input.is_action_just_released("boost"):
-		set_state(previous_state)
-	
-	if state not in [States.BOOSTING] and Input.is_action_just_pressed("dash"):
-		set_state(States.DASHING)
-		
-	## DASH STATE HERE ##
-	
-	if state not in [States.BOOSTING] and Input.is_action_just_pressed("toggle-flight"):
-		set_state(States.FLYING_IDLE)
-	
-	
-
-	if ((state not in [States.JUMPING
-			, States.BOOSTING, States.FLYING
-			, States.FLYING_IDLE]) 
-			and is_on_floor() 
-			and Input.is_action_pressed("jump")):
-		set_state(States.GLIDING)
-	
 	if state == States.BOOSTING:
 		_move_direction = ( -camera_forward 
 			+ camera_right * 
 			player_input_direction.x)
-		velocity = velocity.move_toward(_move_direction 
-			* flight_speed, flight_acceleration 
-			* delta)
+			
+		if Input.is_action_just_released("boost"):
+			set_state(previous_state)
 	
-	if Input.is_action_pressed("boost"):
-		boosting = true
-	else:
-		boosting = false
+	if state not in [States.BOOSTING, States.DASHING] and Input.is_action_just_pressed("dash"):
+		set_state(States.DASHING)
 		
-	_rotate_player_model(delta)
-	_handle_movement(delta)
-
-func _handle_movement(delta):
+	## DASH STATE HERE ##
 	
-	var player_input_direction = Input.get_vector("left", "right"
-		, "forward", "back")
-	
-	var camera_forward = _camera.global_basis.z
-	var camera_right = _camera.global_basis.x
-	
-	if not boosting:
-		if Input.is_action_pressed("jump") and not is_on_floor() and velocity.y <= 0:
-			velocity.y *= glide_speed
+	if state not in [States.BOOSTING]:
+		if Input.is_action_just_pressed("toggle-flight"):
+			if state in [States.FLYING_IDLE, States.FLYING]:
+				set_state(States.FALLING)
+			else:
+				set_state(States.FLYING_IDLE)
+				
 		_move_direction = camera_forward * player_input_direction.y + camera_right * player_input_direction.x
-		_move_direction.y = 0.0
 		_move_direction = _move_direction.normalized()
-
-		var y_velocity := velocity.y
-		velocity.y = 0.0
+	
+	if state not in [ States.BOOSTING, States.FLYING_IDLE, States.FLYING]:
+		_move_direction.y = 0.0
 		
-		velocity = velocity.move_toward(_move_direction * movement_speed
-				, acceleration * delta)
-		
-		velocity.y = y_velocity + gravity * delta
-		
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = jump_impulse
-		
-		if Input.is_action_just_pressed("dash"):
-			_dash()
-
-	else:
-		_move_direction = ( -camera_forward 
-			+ camera_right * 
-			player_input_direction.x)
-		velocity = velocity.move_toward(_move_direction * flight_speed, flight_acceleration * delta)
+	if state in [States.FLYING_IDLE, States.FLYING]:
+		if Input.is_action_pressed("jump"):
+			_move_direction.y = Vector3.UP.normalized().y
+		if Input.is_action_pressed("down"):
+			_move_direction.y = Vector3.DOWN.normalized().y
+			
+	velocity = velocity.move_toward(_move_direction * applied_movement_speed, applied_acceleration * delta)
+	
+	velocity.y = velocity.y + applied_gravity * delta
+	
+	if state == States.JUMPING:
+		velocity.y = jump_impulse
+		set_state(States.FALLING)
 
 	move_and_slide()
+	_rotate_player_model(delta)
 
-	print("current speed: ", velocity.length())
+	print(velocity.y)
 
 func _rotate_player_model(delta):
 	if _move_direction.length() > 0.0:
@@ -160,23 +128,32 @@ func _rotate_player_model(delta):
 		, Vector3.UP)
 	_skin.global_rotation.y = lerp_angle(_skin.rotation.y
 		, target_angle, rotation_speed * delta)
-		
-func _dash():
-	var y_velocity = velocity.y
-	velocity = velocity.normalized() * (velocity.length() + dash_impulse)
-	velocity.y = y_velocity
-	
+
 func set_state(new_state: int) -> void:
-	previous_state = state
-	state = new_state
 	
-	if previous_state in [States.GLIDING, States.FLYING_IDLE, States.FLYING]:
+	if state != new_state:
+		previous_state = state
+		state = new_state
+	
+	if previous_state in [States.GLIDING, States.BOOSTING, States.FLYING_IDLE, States.FLYING]:
 		applied_gravity = gravity
+		
+	if previous_state in [States.FLYING_IDLE, States.FLYING, States.BOOSTING]:
+		applied_movement_speed = movement_speed
+		applied_acceleration = acceleration
 	
-	if state == States.GLIDING:
+	if new_state == States.GLIDING:
 		applied_gravity = gravity * glide_speed
 	
-	if state in [States.FLYING, States.FLYING_IDLE]:
-		applied_gravity = 0
+	if new_state in [States.FLYING, States.FLYING_IDLE]:
+		applied_gravity = 0.0
+		applied_movement_speed = flight_speed
+		applied_acceleration = flight_acceleration
+	
+	if new_state in [States.BOOSTING]:
+		applied_gravity = 0.0
+		applied_movement_speed = boost_speed
+		applied_acceleration = boost_acceleration
 
-	print(state)
+	print(States.find_key(state))
+	print(applied_gravity)
